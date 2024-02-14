fb7dd2ca23d804a2e63076f1684f263909fa5aa8
Tologon Abakirov <tologon.03.06.2003@gmail.com>
2024-02-14 15:09:28 +0600

    Отключено ХП и Триггеры

--- sms/raw_sale/models.py
+++ sms/raw_sale/models.py
@@ -2,12 +2,13 @@ from django.db import models
 from positions.models import Positions
 from Rawmaterials.models import RawMaterials
 from employees.models import Employees
+from datetime import date
 # Create your models here.
 class RawMaterialPurchase(models.Model):
     RawMaterial_id = models.ForeignKey(RawMaterials, on_delete=models.CASCADE, db_column='RawMaterial_id')
     Quantity = models.FloatField()
     Amount = models.DecimalField(max_digits=19, decimal_places=2)
-    Date = models.DateField()
+    Date = models.DateField(default=date.today)  # Set default to today's date
     Employee_id = models.ForeignKey(Employees, on_delete=models.CASCADE, db_column='Employee_id')
 
     class Meta:
--- sms/raw_sale/views.py
+++ sms/raw_sale/views.py
@@ -6,6 +6,10 @@ from datetime import date
 from django.db import connection
 from django.contrib import messages
 from django.http import HttpResponseServerError
+from budget.models import Budget
+from Rawmaterials.models import RawMaterials
+
+
 
 def list(request):
     if request.method == 'POST':
@@ -27,47 +31,49 @@ def detail(request, pk):
     employee = get_object_or_404(RawMaterialPurchase, pk=pk)
     return render(request, 'rawSale_detail.html', {'employee': employee})
 
-# def create(request):
-#     if request.method == 'POST':
-#         form = EmployeeForm(request.POST)
-#         if form.is_valid():
-#             employee = form.save(commit=False)
-#             employee.save()
-#             return redirect('rawSale_detail', pk=employee.pk)
-#     else:
-#         form = EmployeeForm()
-#     return render(request, 'rawSale_form.html', {'form': form})
+# def check_budget_enough(amount):
+#     try:
+#         with connection.cursor() as cursor:
+#             cursor.execute("DECLARE @result INT; EXEC @result = IsBudgetEnough @amount=%s; SELECT @result;", [amount])
+#             result = cursor.fetchone()
+#             if result:
+#                 return result[0]
+#     except Exception as e:
+#         print(f"Error executing SQL: {e}")
+#         return -1
+#     return -1
 
-def check_budget_enough(amount):
-    try:
-        with connection.cursor() as cursor:
-            cursor.execute("DECLARE @result INT; EXEC @result = IsBudgetEnough @amount=%s; SELECT @result;", [amount])
-            result = cursor.fetchone()
-            if result:
-                return result[0]
-    except Exception as e:
-        print(f"Error executing SQL: {e}")
-        return -1
-    return -1
+def update_budget(amount):
+    budget = Budget.objects.first()
+    budget.Budget_Amount -= amount
+    budget.save()
+
+def update_raw_materials_quantity(raw_material_id, amount, quantity):
+    raw_material = RawMaterials.objects.get(id=raw_material_id)
+    raw_material.Quantity += quantity
+    raw_material.Amount += amount
+    raw_material.save()
 
 def create(request):
     if request.method == 'POST':
         form = EmployeeForm(request.POST)
         if form.is_valid():
             amount = form.cleaned_data['Amount']
-            # with connection.cursor() as cursor:
-            #     cursor.execute("EXEC [dbo].[IsBudgetEnough] @amount=%s", [amount])
-            #     result = cursor.fetchone()
-            # is_budget_enough = result[0] == 0
-            is_budget_enough = check_budget_enough(amount)
+            quantity = form.cleaned_data['Quantity']
+            raw_material_id = form.cleaned_data['RawMaterial_id'].id
+            budget = Budget.objects.first()
+            is_budget_enough = budget.Budget_Amount<amount
+
             if not is_budget_enough:
                 employee = form.save(commit=False)
                 employee.save()
+                update_budget(amount)
+                update_raw_materials_quantity(raw_material_id, amount, quantity)
                 return redirect('rawSale_list')
             else:
                 messages.error(request, 'Бюджет недостаточен для проведения операции.')
         else:
-            # Ваша логика, если форма невалидна
+            # если форма невалидна
             messages.error(request, 'Форма содержит ошибки.')
     else:
         form = EmployeeForm()
